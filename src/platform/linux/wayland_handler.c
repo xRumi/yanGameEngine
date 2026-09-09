cd8c6c8b5360af276c38ec2866d0d138c2c5c0f9
Rumi <mhxrumi@gmail.com>
2026-09-09 21:22:18 +0600

    remove duplicates for linux wayland build

--- src/platform/linux/wayland_handler.c
+++ src/platform/linux/wayland_handler.c
@@ -350,17 +350,6 @@ void platformShutdown() {
 };
 
 void platformConsoleWrite(const char* message, uint8_t color);
-double platformGetTime() {
-    struct timespec ts;
-    clock_gettime(CLOCK_MONOTONIC, &ts);
-    return ts.tv_sec + ts.tv_nsec * 1e-9;
-}
-void platformSleep(double time) {
-    struct timespec ts;
-    ts.tv_sec = (time_t)time;
-    ts.tv_nsec = (time - ts.tv_sec) * 1e9;
-    nanosleep(&ts, NULL);
-}
 
 bool platformInputIsKeyDown(KeyboardInputMap key) {
     return hashmap_get(internalStatePlatform.keyboard_input_xkb, keyboard_input_xkb_map[key]);
